c8b3c6e4413bb197f1830e00664ebf32c23da178
fnlctrl <fnlctrl@gmail.com>
2014-03-11 22:37:38 +0800

    8#

--- js/main.js
+++ js/main.js
@@ -1,12 +1,92 @@
 $(document).ready(function() {
-	//
-	$('#background').hide()
-	$('#stripes').hide()
-	function show() {
-		$('#background').show('slow')
-		$('#stripes').show('slow')
-	}
-	$('body').animate({opacity:1},1600,show)
+	//加载动画
+	$(window).bind('load',function () {
+		$('#background').hide()
+		$('#stripes').hide()
+		function show() {
+			$('#background').show('slow')
+			$('#stripes').show('slow')
+		}
+		$('body').animate({opacity:1},1600,show)
+	})
+	//自适应高度
+	$(window).bind('load resize scroll',function() {
+		window.h = $(window).height()
+		window.w = $(window).width()
+		$('.page').height(h)
+		$('.pageContent').height(0.9*h)
+		$('.intro').height(0.8*h)
+		$('#page0').width(w).height(h)
+		$('#navContainer').css('top',h-50)
+		//是否显示左箭头
+		if ($(window).scrollLeft() > window.w-10 ) {
+			$('#svg_arrowLeft').show()
+		}
+		else {
+			$('#svg_arrowLeft').hide()
+		}
+		//检测页面位置
+		window.anchor = []
+		window.navAnchor = []
+		anchor[0]=0
+		navAnchor[0]=0
+		for (var i=0,len=$('.page').length;i<len;i++) {
+			anchor.push($('.page').eq(i).offset().left)
+		}
+		for (var i=0,len=$('.intro').length;i<len;i++) {
+			navAnchor.push($('.intro').eq(i).offset().left)
+		}
+		for (var i=0,len=anchor.length;i<len;i++) {
+			if ($(window).scrollLeft() > anchor[i]-1) {
+				window.currentPage = i
+			}
+		}
+		for (var i=0,len=navAnchor.length;i<len;i++) {
+			if ($(window).scrollLeft() > navAnchor[i]-1) {
+				window.currentGroup = i
+			}
+		}
+		console.log(window.currentPage+"　"+window.currentGroup)
+		//检测导航栏当前值
+		$('.nav').each(function() {
+			if ( window.currentGroup == $(this).attr('data') ) {
+				var rgb = $(this).css('border-color').match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+				$(this).css('background-color','rgb('+rgb[1]+','+rgb[2]+','+rgb[3]+')')
+			}
+			else {
+				$(this).css('background-color','')
+			}
+		})
+		//播放器移动
+		var playerInitialTop = parseInt($('#titleImage').css('top')) + 393
+		var playerTopDelta = $(window).scrollLeft() / 2.5
+		$('#player').css('top',playerInitialTop)
+		if ( (playerInitialTop + playerTopDelta < window.h-110) & (window.h>570) ) {
+			$("#player").css('top', playerInitialTop + playerTopDelta )
+		}
+		else if (window.h<570){
+			$("#player").css('top',playerInitialTop)
+		}
+		else {
+			$("#player").css('top',window.h-110)
+		}
+	})
+	//导航栏
+	$('.nav').click(function() {
+		var destination = $(this).attr('data')
+		$('html body').animate({scrollLeft:navAnchor[destination]},500)
+	})
+	//左右方向按钮
+	$('#svg_arrowRight').click(function() {
+		$('html body').animate({scrollLeft:anchor[currentPage+1]},500)
+		currentPage ++
+		console.log(window.currentPage)
+	})
+	$('#svg_arrowLeft').click(function() {
+		$('html body').animate({scrollLeft:anchor[currentPage-1]},500)
+		currentPage --
+		console.log(window.currentPage)
+	})
 	//Replace all SVG images with inline SVG
 	jQuery('img.svg').each(function(){
 		var $img = jQuery(this);
@@ -30,68 +110,11 @@ $(document).ready(function() {
 			$img.replaceWith($svg);
 		}, 'xml');
 	});
-	//滚动页面
-	$('#svg_arrowRight').click(function() {
-		$('html body').animate({scrollLeft:anchor[currentPage+1]},500)
-		currentPage ++
-		console.log(window.currentPage)
-	})
-	$('#svg_arrowLeft').click(function() {
-		$('html body').animate({scrollLeft:anchor[currentPage-1]},500)
-		currentPage --
-		console.log(window.currentPage)
-	})
-	//导航栏
-	
+
+
+
+
+
+
 })
-//播放器移动
-$(window).bind('load scroll resize',function() {
-	var playerInitialTop = parseInt($('#titleImage').css('top')) + 393
-	var playerTopDelta = $(window).scrollLeft() / 2.5
-	$('#player').css('top',playerInitialTop)
-	if ( (playerInitialTop + playerTopDelta < window.h-110) & (window.h>570) ) {
-		$("#player").css('top', playerInitialTop + playerTopDelta )
-	}
-	else if (window.h<570){
-		$("#player").css('top',playerInitialTop)
-	}
-	else {
-		$("#player").css('top',window.h-110)
-	}
-})
-//自适应高度
-$(window).bind('load resize scroll',function() {
-	window.h = $(window).height()
-	window.w = $(window).width()
-	$('.page').height(h)
-	$('.pageContent').height(0.9*h)
-	$('#page0').width(w).height(h)
-	$('.intro').height(0.8*h)
-	var scale = h/650
-//		$('.article').css('-webkit-transform','scale('+scale+')')
-	$('#nav').css('top',h-50)
-//			$('.pageContent[scale=true]').each(function () {
-//				var imgWidth = $(this).find('img').first().outerWidth(true)
-//				var articleWidth = $(this).find('.article').first().outerWidth(true)
-//				$(this).width(imgWidth+articleWidth*scale+1000*scale)
-//			})
-	//是否显示左箭头
-	if ($(window).scrollLeft() > window.w-10 ) {
-		$('#svg_arrowLeft').show()
-	}
-	else {
-		$('#svg_arrowLeft').hide()
-	}
-	//检测页面位置
-	window.anchor = []
-	anchor[0]=0
-	for (var i=0,len=$('.page').length;i<len;i++) {
-		anchor.push($('.page').eq(i).offset().left)
-	}
-	for (var i=0,len=$('.page').length;i<len;i++) {
-		if ( ($(window).scrollLeft() < 	anchor [i+1] + 1) & ($(window).scrollLeft() > anchor[i] -1)) {
-			window.currentPage = i
-		}
-		console.log(window.currentPage)
-	}
-})
+
