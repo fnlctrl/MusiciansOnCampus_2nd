eca85962b7a53b8b0b35d452eec3a0485d4c229a
fnlctrl <fnlctrl@gmail.com>
2014-03-14 21:27:30 +0800

    21#
    added some cute animations
    firefox and ie not working properly

--- js/main.js
+++ js/main.js
@@ -27,6 +27,7 @@ $(document).ready(function() {
 			$img.replaceWith($svg);
 		}, 'xml');
 	});
+	//
 	$(window).bind('load resize scroll',function() {
 		//Set width and height
 		window.h = $(window).height()
@@ -42,6 +43,41 @@ $(document).ready(function() {
 			bodyWidth += $(this).outerWidth()
 		})
 		$('body').width(bodyWidth+10)
+		//Detect current page and group
+		window.anchor = []
+		window.navAnchor = []
+		anchor[0]=0
+		navAnchor[0]=0
+		for (var i=0,len=$('.page').length;i<len;i++) {
+			anchor.push($('.page').eq(i).offset().left)
+		}
+		for (var i=0,len=$('.intro').length;i<len;i++) {
+			navAnchor.push($('.intro').eq(i).offset().left)
+		}
+		for (var i=0,len=anchor.length;i<len;i++) {
+			if ($(window).scrollLeft() > anchor[i]-20) {
+				window.currentPage = i
+			}
+		}
+		for (var i=0,len=navAnchor.length;i<len;i++) {
+			if ($(window).scrollLeft() > navAnchor[i]-20) {
+				window.currentGroup = i
+			}
+		}
+		//Show or hide left arrow
+		if ($(window).scrollLeft() > window.w-10 ) {
+			$('#arrowLeft').show()
+		}
+		else {
+			$('#arrowLeft').hide()
+		}
+		//Show or hide right arrow
+		if ($(window).scrollLeft() < $('body').width() - 2000 ) {
+			$('#arrowRight').show()
+		}
+		else {
+			$('#arrowRight').hide()
+		}
 		//Move player according to how much the page is scrolled
 		var playerInitialTop = parseInt($('#titleImage').css('top')) + 393
 		var playerTopDelta = $(window).scrollLeft() / 2.5
@@ -55,7 +91,7 @@ $(document).ready(function() {
 		else {
 			$("#player").css('top',window.h-110)
 		}
-		//detect current page and group
+		//Detect current page and group
 		window.anchor = []
 		window.navAnchor = []
 		anchor[0]=0
@@ -86,34 +122,54 @@ $(document).ready(function() {
 				$(this).css('background-color','')
 			}
 		})
+		//Hightlight arrows on hover
+		$('.arrow')
+		.mouseenter(function() {
+			$(this).find('polygon').css('fill',$('.arrow').find('path').css('fill'))
+		})
+		.mouseleave(function() {
+			$(this).find('polygon').css('fill','#FFF')
+		})
 		//Change color of arrow according to current group
 		$('.arrow').each(function() {
 			if (window.currentGroup == 0) {
 				$(this).find('path').css('fill','#E6067C')
-				$(this).find('polygon').css('fill','#FFF')
+				if ($(this).is(":hover")) {
+					$(this).find('polygon').css('fill','#E6067C')
+				}
 			}
 			else if (window.currentGroup == 1) {
 				$(this).find('path').css('fill','#2980B9')
-				$(this).find('polygon').css('fill','#FFF')
+				if ($(this).is(":hover")) {
+					$(this).find('polygon').css('fill','#2980B9')
+				}
 			}
 			else if (window.currentGroup == 2) {
 				$(this).find('path').css('fill','#39B54A')
-				$(this).find('polygon').css('fill','#FFF')
+				if ($(this).is(":hover")) {
+					$(this).find('polygon').css('fill','#39B54A')
+				}
 			}
 			else if (window.currentGroup == 3) {
 				$(this).find('path').css('fill','#EA2E49')
-				$(this).find('polygon').css('fill','#FFF')
+				if ($(this).is(":hover")) {
+					$(this).find('polygon').css('fill','#EA2E49')
+				}
 			}
 			else if (window.currentGroup == 4) {
 				$(this).find('path').css('fill','#C24704')
-				$(this).find('polygon').css('fill','#FFF')
+				if ($(this).is(":hover")) {
+					$(this).find('polygon').css('fill','#C24704')
+				}
 			}
 			else if (window.currentGroup == 5) {
 				$(this).find('path').css('fill','#00994C')
-				$(this).find('polygon').css('fill','#FFF')
+				if ($(this).is(":hover")) {
+					$(this).find('polygon').css('fill','#00994C')
+				}
 			}
 		})
-		//switch background music according to current group
+		//Switch background music according to current group
 		if (window.currentGroup==0){
 			if (window.currentTrack!=0) {
 				$('#audio').animate({volume:0},1000)	
@@ -152,13 +208,20 @@ $(document).ready(function() {
 		}
 	})
 	//Animation at first page load
-	$(window).bind('load',function() {
+	$(window).bind('load',function () {
 		$('#background').width(0)
+	//	$('#stripes').hide()
 		function pageInit() {
 			$('#background').animate({width:w},800)
 			document.getElementById('audio').play()
+			stripesAnimation()
+		}
+		function stripesAnimation() {
+			$('#stripes path').attr('class','stripesPath')
+	//		$('#stripes').show()
 		}
 		$('body').animate({opacity:1},1600,pageInit)
 	})
 })
+
 
--- js/nav.js
+++ js/nav.js
@@ -1,45 +1,23 @@
-$(window).bind('load resize scroll',function() {
-	//Show or hide left arrow
-	if ($(window).scrollLeft() > window.w-10 ) {
-		$('#arrowLeft').show()
-	}
-	else {
-		$('#arrowLeft').hide()
-	}
-	//Show or hide right arrow
-	if ($(window).scrollLeft() < $('body').width() - 2000 ) {
-		$('#arrowRight').show()
-	}
-	else {
-		$('#arrowRight').hide()
-	}
-})
-$(window).bind('load',function() {
+$(window).bind('load',function () {
 	//Scroll left or right by clicking arrows
 	$('#arrowRight').click(function() {
-		$('html,body').animate({scrollLeft:anchor[currentPage+1]},500)
+		$('html,body').animate({scrollLeft:anchor[window.currentPage+1]},500)
 		if (currentPage < anchor.length-1) {
-			currentPage ++
+			window.currentPage ++
 		}
-	//	console.log(window.currentPage)
+		$(this).find('polygon').css('fill',window.color)
+		console.log(window.currentPage)
 	})
 	$('#arrowLeft').click(function() {
-		$('html,body').animate({scrollLeft:anchor[currentPage-1]},500)
-		currentPage --
-	//	console.log(window.currentPage)
+		$('html,body').animate({scrollLeft:anchor[window.currentPage-1]},500)
+		window.currentPage --
+		$(this).find('polygon').css('fill',window.color)
+		console.log(window.currentPage)
 	})
 	//Fast navigate to groups
 	$('.nav').click(function() {
 		var destination = $(this).attr('data')
 		$('html,body').animate({scrollLeft:navAnchor[destination]},500)
 	})
-	//Hightlight arrows on hover
-	$('.arrow')
-	.mouseenter(function() {
-		var color = $(this).find('path').css('fill')
-		$(this).find('polygon').css('fill',color)
-	})
-	.mouseleave(function() {
-		$(this).find('polygon').css('fill','#FFF')
-	})
 })
+
